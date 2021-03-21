8015b2d489b4f3544e3a50e5c3884fc09bc15e46
Moreen <moreenkim36@gmail.com>
2021-03-21 22:44:53 +0300

    Create the function, add styling for the search pag using ant design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { Layout, Menu } from 'antd';
+import { Route, Switch } from 'react-router';
+
+import Issues from './container/Issues';
+
+import 'antd/dist/antd.css';
 import './App.css';
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="/">Issue Tracker</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Switch>
+          <Route exact path="/" component={Issues} />
+        </Switch>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Issue Tracker ©2021 Created by Moreen Kim
+      </Footer>
+    </Layout>
   );
 }
 
