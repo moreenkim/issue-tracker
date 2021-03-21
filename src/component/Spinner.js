2851cd832acc42f8c17b12720c6b06de89e4dda3
Moreen <moreenkim36@gmail.com>
2021-03-21 22:48:36 +0300

    Create a searchbar using antstyling, a spinner for loading wait time and a spinner css id styling

---
+++ src/component/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Spin } from 'antd';
+
+import './Spinner.css';
+
+const Spinner = () => {
+  return (
+    <div id="spinner">
+      <Spin />
+    </div>
+  );
+};
+
+export default Spinner;
