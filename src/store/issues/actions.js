658dafdc969571c764cf17da4ad5f90da59f2c45
Moreen <moreenkim36@gmail.com>
2021-03-28 16:50:39 +0300

    Add actions for the loading and labels functionalities

--- src/store/issues/actions.js
+++ src/store/issues/actions.js
@@ -10,8 +10,58 @@ export function getRepoIssuesAction(payload) {
         type: types.GET_ISSUES,
         issues,
       });
+      dispatch({
+        type: types.SET_LOADING,
+        loading: false,
+      });
+    } catch (error) {
+      dispatch({
+        type: types.SET_LOADING,
+        loading: false,
+      });
+      console.error(error);
+    }
+  };
+}
+
+export function getRepoLabelsAction(payload) {
+  return async (dispatch) => {
+    try {
+      const labels = await IssueService.getRepoLabels(payload);
+
+      dispatch({
+        type: types.GET_LABELS,
+        labels,
+      });
     } catch (error) {
       console.error(error);
     }
+  };
+}
+
+export function setLoadingAction(status) {
+  return async (dispatch) => {
+    dispatch({
+      type: types.SET_LOADING,
+      loading: status,
+    });
+  };
+}
+
+export function setSelectedLabelsAction(selectedLabels) {
+  return async (dispatch) => {
+    dispatch({
+      type: types.SET_SELECTED_LABELS,
+      selectedLabels,
+    });
+  };
+}
+
+export function setLabelsAction(labels) {
+  return async (dispatch) => {
+    dispatch({
+      type: types.GET_LABELS,
+      labels,
+    });
   };
 }
